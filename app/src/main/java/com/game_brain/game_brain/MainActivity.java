942081964aa578036974eb70c2a359ee8f183696
Game-brain-8 <mizanurrohman360m@gmail.com>
2024-08-15 11:34:44 +0600

    Add files via upload

---
+++ app/src/main/java/com/game_brain/game_brain/MainActivity.java
@@ -0,0 +1,14 @@
+package com.game_brain.game_brain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
